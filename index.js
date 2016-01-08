f9fcd82ba4b016301c420db59f99c43f60ae9bfe
hector <hectorluis.collado@gmail.com>
2016-01-08 14:18:49 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var Promise = require('bluebird')
+
+class Pipeline {
+    constructor(handlers) {
+        if (!handlers)
+            this.handlers = []
+        else
+            this.handlers = handlers
+    }
+
+    use(handler){
+        this.handlers.push(handler)
+        return this
+    }
+
+    start(input, output){
+        if (!output)
+            output = {}
+
+        return new Promise((resolve, reject) => {
+            var index = 0
+
+            var next = function(){
+                if (index == this.handlers.length)
+                    resolve(output)
+                else {
+                    let h = this.handlers[index++]
+                    h(input, output, next)
+                }
+            }.bind(this)
+
+            next()
+        })
+    }
+}
+
+module.exports = Pipeline
